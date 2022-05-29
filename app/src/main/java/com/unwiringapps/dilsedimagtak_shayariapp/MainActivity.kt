b053e51be3e61e750354fd7bed32d4a455748272
thetanmay1 <foundertanmay@gmail.com>
2022-05-29 18:07:49 +0530

    c3

--- app/src/main/java/com/unwiringapps/dilsedimagtak_shayariapp/MainActivity.kt
+++ app/src/main/java/com/unwiringapps/dilsedimagtak_shayariapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.unwiringapps.dilsedimagtak_shayariapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.unwiringapps.dilsedimagtak_shayariapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+
+    }
+
+
+
 }
